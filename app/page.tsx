3b37b8e7695c244749ad68a1499f02e0e64469e2
Yilmaz Kadir <ulas@MacBook-Pro-de-Ulas.local>
2025-07-04 19:45:02 +0200

    🔨add footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import Carte from '@/components/Menu-Section';
 import About from '@/components/About';
 import RestaurantSection from '@/components/Restaurant-Section';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <RestaurantSection />
       <Carte />
       <About />
+      <Footer />
     </PageLayout>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className="bg-foreground py-16 px-4">
+      <div className="mx-auto max-w-6xl">
+        <h1 className="text-white">Epicurien</h1>
+        <p className="text-white">
+          123 Rue de l&apos; élégance <br /> 75000 Ville, France
+        </p>
+        <p className="text-white">+33 1 23 45 67 89</p>
+        <p className="text-white">contact@epicurien.fr</p>
+      </div>
+    </div>
+  );
+}
